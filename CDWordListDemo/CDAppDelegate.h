294d4800255c0602a1528d2be53cab83d86574c3
Connor Duggan <connor.m.duggan@gmail.com>
2012-03-02 05:46:57 -0800

    Initial Commit

---
+++ CDWordListDemo/CDAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  CDAppDelegate.h
+//  CDWordListDemo
+//
+//  Created by Naruto Uzumaki on 12-03-02.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class CDViewController;
+
+@interface CDAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) CDViewController *viewController;
+
+@end
---
+++ CDWordListDemo/CDViewController.h
@@ -0,0 +1,13 @@
+//
+//  CDViewController.h
+//  CDWordListDemo
+//
+//  Created by Naruto Uzumaki on 12-03-02.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CDViewController : UIViewController
+
+@end
